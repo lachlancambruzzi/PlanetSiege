02d4f1c28bd12d08308487d3b0add3c8902d8274
Jon Slayton <138514707+JonSlayton@users.noreply.github.com>
2026-08-02 16:39:59 +1000

    Mouse look fix + Enemy ship
    -Fixed ship not facing mouse direction
    
    -Added enemy ship (only basic functionality for now)

--- Planet Siege/Assets/Lachlan_Scripts/FaceMouse.cs
+++ Planet Siege/Assets/Lachlan_Scripts/FaceMouse.cs
@@ -2,33 +2,15 @@ using UnityEngine;
 
 public class FaceMouse : MonoBehaviour
 {
+    private Vector3 mouseWorldPosition;
+    private Vector2 faceDirection;
+
     void Update()
     {
-        /*
-
-        // Get the mouse position in world space
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0f;
-
-        // Get the direction from the object to the mouse
-        Vector3 direction = mousePos - transform.position;
-
-        // Calculate angle and rotate
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-
-        */
-
-        // 1. Get the mouse position in world space coordinates
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        // 2. Force the Z-plane to match the object's Z position to prevent 3D tilting
+        mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPosition.z = transform.position.z;
 
-        // 3. Calculate the direction vector from the object to the mouse
-        Vector2 faceDirection = mouseWorldPosition - transform.position;
-
-        // 4. Directly assign the up vector to look along this direction
+        faceDirection = mouseWorldPosition - transform.position;
         transform.up = faceDirection;
     }
 }
---
+++ Planet Siege/Assets/Lachlan_Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerShip;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
